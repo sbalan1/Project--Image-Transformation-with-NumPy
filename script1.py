d45c4deda7995e7efaea989c02846ccb58aa27b8
sbalan1 <balanshilpa1@gmail.com>
2024-02-08 00:58:36 -0800

    Update script1.py

--- script1.py
+++ script1.py
@@ -14,7 +14,7 @@ heart_img = np.array([[255,0,0,255,0,0,255],
                   [255,255,0,255/2,0,255,255],
                   [255,255,255,0,255,255,255]])
 
-# This is a helper function that makes it easy for you to show images!
+# This is a helper function that makes it easy for you to show images
 def show_image(image, name_identifier):
   plt.imshow(image, cmap="gray")
   plt.title(name_identifier)
